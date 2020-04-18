65af97cb5b09eff1472fde6298a1cf7adb08a364
mcs\mohamed.s.seliem <mohamed.said.seliem@mcs.gov.sa>
2020-04-18 21:49:33 +0300

    Add project files.

---
+++ Date.UmAlqura/DateModel.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace Date.UmAlqura
+{
+    public class DateModel
+    {
+       
+        public string Date { get; set; }
+
+        [JsonConverter(typeof(StringEnumConverter))]
+        public CalendarType CalendarType { get; set; }
+
+        ///// <summary>
+        ///// allFormats ={"yyyy/MM/dd","yyyy/M/d","dd/MM/yyyy","d/M/yyyy","dd/M/yyyy","d/MM/yyyy","yyyy-MM-dd","yyyy-M-d","dd-MM-yyyy","d-M-yyyy","dd-M-yyyy","d-MM-yyyy","yyyy MM dd","yyyy M d","dd MM yyyy","d M yyyy","dd M yyyy","d MM yyyy"}
+        ///// </summary>
+        //public string DateFormat { get; set; }
+
+    }
+    public class OptionalDateModel
+    {
+        public string Date { get; set; }
+
+        [JsonConverter(typeof(StringEnumConverter))]
+        public CalendarType CalendarType { get; set; }
+    }
+
+    [JsonConverter(typeof(StringEnumConverter))]
+    public enum CalendarType
+    {
+        Gregorian,
+        Hijri
+    }
+}
